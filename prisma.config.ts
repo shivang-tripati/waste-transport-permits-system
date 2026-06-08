e9e9df5bafa2265c12e3174f45121e50dfada38b
shivang-tripati <shivangtripathi101@gmail.com>
2026-06-08 21:58:56 +0530

    fix: DATABASE_URL in prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,5 @@
-// prisma.config.ts
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -9,6 +8,8 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url:
+      process.env.DATABASE_URL ||
+      "postgresql://dummy:dummy@localhost:5432/dummy",
   },
 });
